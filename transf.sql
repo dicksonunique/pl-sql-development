22fb297e9410e9780eb83377e310e859c7202a17
Uwem Dickson <dicksonunique@gmail.com>
2022-10-06 07:09:16 -0700

    transf.sql ddl

---
+++ transf.sql
@@ -0,0 +1,19 @@
+CREATE TABLE transf
+
+(
+
+     id     NUMBER,
+
+     id2    NUMBER,
+
+     col1   VARCHAR2 (1500 BYTE),
+
+     col2   VARCHAR2 (1500 BYTE),
+
+     col3   VARCHAR2 (1500 BYTE),
+
+     col4   DATE
+
+)
+
+/
